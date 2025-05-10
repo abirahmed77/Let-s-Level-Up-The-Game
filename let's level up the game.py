9893242fc5c5033dfe25ca32b952ee9fdff577d4
Abir <abirgpx@gmail.com>
2025-05-10 09:53:12 +0600

    first commit

---
+++ let's level up the game.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+import sys
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 400
+MOVEMENT_SPEED = 5
+FONT_SIZE = 72
+BLACK = pygame.color('black')
+RED = pygame.color('red')
+BLUE = pygame.color('black')
+WHITE = pygame.color('black')
+
